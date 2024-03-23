adf79ef83545aeb62892196f8adbfe9377e7e3c7
koslinj <koslinj@gmail.com>
2024-03-23 18:06:33 +0100

    update CORS settings

--- src/main/java/koslin/jan/shop/WebConfig.java
+++ src/main/java/koslin/jan/shop/WebConfig.java
@@ -12,6 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow requests from any origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedHeaders("*");
     }
 }
