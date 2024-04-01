f6496124a53a05ec5e65917cf68ceaf3a7cab9ee
koslinj <koslinj@gmail.com>
2024-04-01 23:32:02 +0200

    create Opinion controller for fetching opinions connected with specific product

---
+++ src/main/java/koslin/jan/shop/controller/OpinionController.java
@@ -0,0 +1,24 @@
+package koslin.jan.shop.controller;
+
+import koslin.jan.shop.dto.OpinionDto;
+import koslin.jan.shop.service.OpinionService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/opinions")
+public class OpinionController {
+
+    private OpinionService opinionService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<OpinionDto>> getOpinionsByProductId(@PathVariable("id") Long id) {
+        List<OpinionDto> opions = opinionService.getOpinionsByProductId(id);
+        return new ResponseEntity<>(opions, HttpStatus.OK);
+    }
+}
