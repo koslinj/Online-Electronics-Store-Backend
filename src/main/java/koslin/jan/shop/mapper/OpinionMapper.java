5227f6e94f79ed6f485df8f0063f16326864ce12
koslinj <koslinj@gmail.com>
2024-04-02 15:27:43 +0200

    create OpinionMapper

---
+++ src/main/java/koslin/jan/shop/mapper/OpinionMapper.java
@@ -0,0 +1,31 @@
+package koslin.jan.shop.mapper;
+
+import koslin.jan.shop.dto.CategoryDto;
+import koslin.jan.shop.dto.OpinionDto;
+import koslin.jan.shop.entity.Category;
+import koslin.jan.shop.entity.Opinion;
+import koslin.jan.shop.entity.Product;
+import koslin.jan.shop.entity.User;
+
+public class OpinionMapper {
+    public static OpinionDto toDto(Opinion opinion){
+        return new OpinionDto(
+                opinion.getId(),
+                opinion.getStars(),
+                opinion.getContent(),
+                opinion.getCreatedAt(),
+                opinion.getUser().getFirstName()
+        );
+    }
+
+    public static Opinion toEntity(OpinionDto opinionDto, Product product, User user){
+        return new Opinion(
+                opinionDto.getId(),
+                opinionDto.getStars(),
+                opinionDto.getContent(),
+                opinionDto.getCreatedAt(),
+                product,
+                user
+        );
+    }
+}
