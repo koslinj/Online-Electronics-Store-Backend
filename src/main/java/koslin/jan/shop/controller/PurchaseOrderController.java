33a2e996d7d7ea163e4dbcae8f2851c65873776c
koslinj <koslinj@gmail.com>
2024-04-05 21:21:35 +0200

    change url for getting all and providing endpoint for paginational fetching

--- src/main/java/koslin/jan/shop/controller/PurchaseOrderController.java
+++ src/main/java/koslin/jan/shop/controller/PurchaseOrderController.java
@@ -1,10 +1,10 @@
 package koslin.jan.shop.controller;
 
-import koslin.jan.shop.dto.CategoryDto;
-import koslin.jan.shop.dto.OpinionDto;
 import koslin.jan.shop.dto.PurchaseOrderDto;
 import koslin.jan.shop.service.PurchaseOrderService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +18,16 @@ public class PurchaseOrderController {
 
     private PurchaseOrderService purchaseOrderService;
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<PurchaseOrderDto>> getAll() {
         List<PurchaseOrderDto> allOrders = purchaseOrderService.getAll();
         return new ResponseEntity<>(allOrders, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PurchaseOrderDto>> getPurchaseOrders(Pageable pageable) {
+        Page<PurchaseOrderDto> orders = purchaseOrderService.getPurchaseOrders(pageable);
+        return ResponseEntity.ok(orders);
     }
 
     @PostMapping
