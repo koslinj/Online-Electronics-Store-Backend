c5cebf8a6d6b4fbd8fececa755bacc2c1f944a73
koslinj <koslinj@gmail.com>
2024-04-06 09:44:32 +0200

    repository function for getting orders by username

--- src/main/java/koslin/jan/shop/repository/PurchaseOrderRepository.java
+++ src/main/java/koslin/jan/shop/repository/PurchaseOrderRepository.java
@@ -3,5 +3,8 @@ package koslin.jan.shop.repository;
 import koslin.jan.shop.entity.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
+    List<PurchaseOrder> getAllByUserEmail(String username);
 }
